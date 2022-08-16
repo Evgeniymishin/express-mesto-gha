b55189ec6c718beeb4e23e078f5f8236d9168276
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-17 01:48:47 +0300

    feat: Подготовка к деплою на сервер

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,21 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-const allowedCors = [
-  'localhost:3000',
-  'https://mesto.study.nomoredomains.sbs',
-  'https://api.mesto.study.nomoredomains.sbs',
-  'http://mesto.study.nomoredomains.sbs',
-  'http://api.mesto.study.nomoredomains.sbs',
-];
+// const allowedCors = [
+//   'localhost:3000',
+//   'https://mesto.study.nomoredomains.sbs',
+//   'https://api.mesto.study.nomoredomains.sbs',
+//   'http://mesto.study.nomoredomains.sbs',
+//   'http://api.mesto.study.nomoredomains.sbs',
+// ];
 
 module.exports = (req, res, next) => {
   const { methodHttp } = req;
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  // res.header('Access-Control-Allow-Origin', '*');
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
   if (methodHttp === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
