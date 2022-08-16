94287524928defacc6e6e672ee644601b8718e80
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-17 00:40:34 +0300

    feat: Подготовка к деплою на сервер

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,10 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const allowedCors = [
   'localhost:3000',
-  'mesto.study.nomoredomains.sbs',
-  'api.mesto.study.nomoredomains.sbs',
+  'https://mesto.study.nomoredomains.sbs',
+  'https://api.mesto.study.nomoredomains.sbs',
+  'http://mesto.study.nomoredomains.sbs',
+  'http://api.mesto.study.nomoredomains.sbs',
 ];
 
 module.exports = (req, res, next) => {
