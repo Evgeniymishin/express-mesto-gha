94a6c69e8559ec0f7042d9a3cc4c0258625990d2
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-16 12:34:57 +0300

    fix: Исправлены ошибки автотеста

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,8 +2,6 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../utils/constants');
 const UnauthorizedError = require('../errors/unauthorized');
 
-const handleAuthError = (req, res, next) => next(new UnauthorizedError('Необходима авторизация'));
-
 module.exports = (req, res, next) => {
   const token = req.cookies.access_token;
   let payload;
@@ -11,7 +9,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    return handleAuthError(res);
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   req.user = payload;
