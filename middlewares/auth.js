82ca0b6db535a272887cc6b8f410bc8371445e27
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-17 13:36:18 +0300

    feat: Подготовка к деплою на сервер

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,15 +6,12 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.access_token;
-  console.log(token);
-  console.log(NODE_ENV);
-  console.log(JWT_SECRET);
   let payload;
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : SECRET_KEY);
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError(`${token} ${NODE_ENV} ${JWT_SECRET}`);
   }
 
   req.user = payload;
