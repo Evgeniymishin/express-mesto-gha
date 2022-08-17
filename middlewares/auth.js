70be4e7c3c45e48e74a412a85b30b55ffa99a2d7
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-17 15:19:15 +0300

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,23 +2,21 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../utils/constants');
 const UnauthorizedError = require('../errors/unauthorized');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  let token = req.cookies.access_token;
+  // let token = req.cookies.access_token;
   const { authorization } = req.headers;
-  if (token === undefined) {
-    if (!authorization || !authorization.startWith('Bearer ')) {
-      throw new UnauthorizedError(`${token} ${NODE_ENV} ${JWT_SECRET}`);
-    }
+  if (!authorization || !authorization.startWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
   }
-  token = authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    throw new UnauthorizedError(`${token} ${NODE_ENV} ${JWT_SECRET}`);
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   req.user = payload;
