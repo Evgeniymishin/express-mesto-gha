7dd11bfc37331d12f0b860733e6cdde3deefa376
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-18 16:51:23 +0300

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : SECRET_KEY);
   } catch (err) {
-    throw new UnauthorizedError(`Необходима авторизация ${token}`);
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   req.user = payload;
