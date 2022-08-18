21201daed717ee9205f6a9ce360f77d72d93b729
Evgeniy Mishin <dubreak@yandex.ru>
2022-08-18 14:25:37 +0300

    test

--- controllers/card.js
+++ controllers/card.js
@@ -5,7 +5,7 @@ const ForbiddenError = require('../errors/no-acess');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.send(cards))
     .catch(next);
 };
 
